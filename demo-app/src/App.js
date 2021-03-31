804efadcf5511b3d22fd23c7817a0df3c0a98c48
Umut Akin <umutakin@fastmail.com>
2021-03-31 18:40:23 +0300

    react complete guide 060 - flexible lists

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -15,14 +15,24 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangeHandler = ( event ) => {
+  nameChangedHandler = ( event, id ) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState( {
-      persons: [
-        { name: 'umut', age: 35 },
-        { name: event.target.value, age: 35 },
-        { name: 'memo', age: 34 }
-      ]
-    })
+      persons: persons
+    });
   }
 
   deletePersonHandler = (personIndex) => {
@@ -57,7 +67,8 @@ class App extends Component {
               key={person.id}
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
-              age={person.age}/>
+              age={person.age}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           }) }
         </div>
       );
