d1c4d2d7d6234154266792281d329e25649efe8d
Umut Akin <umutakin@fastmail.com>
2021-04-08 19:03:03 +0300

    react complete guide 071 more on styled components

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover { 
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
 
@@ -36,7 +51,6 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
     const persons = [...this.state.persons]; // spread operator, more modern
     persons.splice(personIndex, 1);
     this.setState( {
@@ -50,18 +64,18 @@ class App extends Component {
 
   render () {
 
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': { 
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': { 
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // };
 
     let persons = null;
     if ( this.state.showPersons ) {
@@ -78,11 +92,11 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = { 
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      // style.backgroundColor = 'red';
+      // style[':hover'] = { 
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
     }
 
     const classes = [];
@@ -93,14 +107,14 @@ class App extends Component {
       classes.push('bold'); // classes = ['red', 'bold']
     }
 
-
     return(
       <div className="App">
         <h1>hi, i am a react app</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <StyledButton
+          onClick={this.togglePersonsHandler}>
+            Toggle Persons
+        </StyledButton>
         { persons }
       </div>
     );
