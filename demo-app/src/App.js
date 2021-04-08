4f74d1782aa352e0771abbf606b8fd6312258cc7
Umut Akin <umutakin@fastmail.com>
2021-04-08 18:51:15 +0300

    react complete guide 070 introducing styled components

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, { StyleRoot } from 'radium';
 import './App.css';
 
 import Person from './Person/Person';
@@ -96,18 +95,16 @@ class App extends Component {
 
 
     return(
-      <StyleRoot>
-        <div className="App">
-          <h1>hi, i am a react app</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button 
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          { persons }
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>hi, i am a react app</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        { persons }
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
