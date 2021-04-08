ee125ae40d1f102e39fcd5519e971c3246046a82
Umut Akin <umutakin@fastmail.com>
2021-04-08 18:37:24 +0300

    react complete guide 069 using radium for media queries

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
 
 import Person from './Person/Person';
@@ -96,14 +96,16 @@ class App extends Component {
 
 
     return(
-      <div className="App">
-        <h1>hi, i am a react app</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { persons }
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>hi, i am a react app</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          { persons }
+        </div>
+      </StyleRoot>
     );
   }
 }
--- demo-app/src/Person/Person.js
+++ demo-app/src/Person/Person.js
@@ -4,8 +4,13 @@ import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>i am {props.name} and i am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type='text' onChange={props.changed} value={props.name} />
